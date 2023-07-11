c1734b358c86124f1a4904c7ea02cfc05b9df397
Ali Abbas <48610822+alivault@users.noreply.github.com>
2023-07-11 16:13:37 -0700

    functional programming

--- script.js
+++ script.js
@@ -5,72 +5,124 @@ import {
   revealTile,
   checkWin,
   checkLose,
-} from './minesweeper.js'
+  positionMatch,
+  markedTilesCount,
+} from './minesweeper.js';
 
-const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 10
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 10;
 
-const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
-const boardElement = document.querySelector('.board')
-const minesLeftText = document.querySelector('[data-mine-count]')
-const messageText = document.querySelector('.subtext')
+let board = createBoard(
+  BOARD_SIZE,
+  getMinePositions(BOARD_SIZE, NUMBER_OF_MINES)
+);
+const boardElement = document.querySelector('.board');
+const minesLeftText = document.querySelector('[data-mine-count]');
+const messageText = document.querySelector('.subtext');
 
-board.forEach(row => {
-  row.forEach(tile => {
-    boardElement.append(tile.element)
-    tile.element.addEventListener('click', () => {
-      revealTile(board, tile)
-      checkGameEnd()
-    })
-    tile.element.addEventListener('contextmenu', e => {
-      e.preventDefault()
-      markTile(tile)
-      listMinesLeft()
-    })
-  })
-})
+function render() {
+  boardElement.innerHTML = '';
+  checkGameEnd();
 
-boardElement.style.setProperty('--size', BOARD_SIZE)
-minesLeftText.textContent = NUMBER_OF_MINES
+  getTileElements(board).forEach(element => {
+    boardElement.append(element);
+  });
+
+  listMinesLeft();
+}
+
+function getTileElements() {
+  return board.flatMap(row => {
+    return row.map(tileToElement);
+  });
+}
+
+function tileToElement(tile) {
+  const element = document.createElement('div');
+  element.dataset.status = tile.status;
+  element.dataset.x = tile.x;
+  element.dataset.y = tile.y;
+  element.textContent = tile.adjacentMinesCount || '';
+  return element;
+}
+
+boardElement.addEventListener('click', e => {
+  if (!e.target.matches('[data-status]')) return;
+
+  board = revealTile(board, {
+    x: parseInt(e.target.dataset.x),
+    y: parseInt(e.target.dataset.y),
+  });
+  render();
+});
+
+boardElement.addEventListener('contextmenu', e => {
+  if (!e.target.matches('[data-status]')) return;
+
+  e.preventDefault();
+  board = markTile(board, {
+    x: parseInt(e.target.dataset.x),
+    y: parseInt(e.target.dataset.y),
+  });
+  render();
+});
+
+boardElement.style.setProperty('--size', BOARD_SIZE);
+render();
 
 function listMinesLeft() {
-  const markedTilesCount = board.reduce((count, row) => {
-    return (
-      count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
-    )
-  }, 0)
-
-  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount(board);
 }
 
 function checkGameEnd() {
-  const win = checkWin(board)
-  const lose = checkLose(board)
+  const win = checkWin(board);
+  const lose = checkLose(board);
 
   if (win || lose) {
-    boardElement.addEventListener('click', stopProp, { capture: true })
-    boardElement.addEventListener('contextmenu', stopProp, { capture: true })
+    boardElement.addEventListener('click', stopProp, { capture: true });
+    boardElement.addEventListener('contextmenu', stopProp, { capture: true });
   }
 
   if (win) {
-    messageText.textContent = 'You Win'
+    messageText.textContent = 'You Win';
   }
   if (lose) {
-    messageText.textContent = 'You Lose'
+    messageText.textContent = 'You Lose';
     board.forEach(row => {
       row.forEach(tile => {
-        if (tile.status === TILE_STATUSES.MARKED) markTile(tile)
-        if (tile.mine) revealTile(board, tile)
-      })
-    })
+        if (tile.status === TILE_STATUSES.MARKED) board = markTile(board, tile);
+        if (tile.mine) board = revealTile(board, tile);
+      });
+    });
   }
 }
 
 function stopProp(e) {
-  e.stopImmediatePropagation()
+  e.stopImmediatePropagation();
+}
+
+function getMinePositions(boardSize, numberOfMines) {
+  const positions = [];
+
+  while (positions.length < numberOfMines) {
+    const position = {
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
+    };
+
+    if (!positions.some(positionMatch.bind(null, position))) {
+      positions.push(position);
+    }
+  }
+
+  return positions;
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
 }
 
 // reset board
 document.querySelector('.reset').addEventListener('click', () => {
-  location.reload()
-})
+  location.reload();
+});
